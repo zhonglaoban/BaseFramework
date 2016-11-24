0b4d8721fa500c3271da9b5ec4f64d269b674fdc
钟凡 <1049056949@>
2016-11-24 16:06:06 +0800

    make some swift file public

--- Example/Pods/Target Support Files/BaseFramework/BaseFramework-umbrella.h
+++ Example/Pods/Target Support Files/BaseFramework/BaseFramework-umbrella.h
@@ -2,6 +2,25 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import <Foundation/Foundation.h>
+#import "PIColorPickerController.h"
+#import "PIDrawerView.h"
+#import "BannerMenuView.h"
+#import "BublleBtn.h"
+#import "FilePath.h"
+#import "TAAbstractDotView.h"
+#import "TAAnimatedDotView.h"
+#import "TADotView.h"
+#import "TAPageControl.h"
+#import "SDCollectionViewCell.h"
+#import "SDCycleScrollView.h"
+#import "UIView+SDExtension.h"
+#import "SNActionSheet.h"
+#import "CacheManager.h"
+#import "NSString+Extension.h"
+#import "UIColor+Extention.h"
+#import "UIImage+Extention.h"
+#import "UIView+Extention.h"
 
 FOUNDATION_EXPORT double BaseFrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char BaseFrameworkVersionString[];
